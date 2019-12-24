7d51508242571fad598cd262cd576558b50767e7
Cody Cahoon <codyjcahoon@gmail.com>
2019-12-23 23:17:09 -0500

    Sets up eslint within project

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,9 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread']
-          }
-        }
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
       },
     ],
   },
